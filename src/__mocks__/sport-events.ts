8c8b60b16113b4bb83f6382fbf1a3d5907c73854
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-03 02:52:30 -0400

    Update sport-events.ts

--- src/__mocks__/sport-events.ts
+++ src/__mocks__/sport-events.ts
@@ -4,65 +4,102 @@ export default [
     "markets": [
       {
         "id": "MKT_1",
-        "name": "Team to Win",
+        "name": "Spread",
         "selections": [
           {
             "id": "SEL_1",
-            "name": "Real Madrid",
-            "price": 1.23
+            "name": "Bucks",
+            "price": +4.5(-110)
           },
           {
             "id": "SEL_2",
-            "name": "Barcelona",
-            "price": 2.05
+            "name": "Celtics",
+            "price": -4.5(-110)
           }
         ]
       },
       {
         "id": "MKT_2",
-        "name": "Player to Score First",
+        "name": "Moneyline",
         "selections": [
           {
             "id": "SEL_3",
-            "name": "Ronaldo",
-            "price": 1.15
+            "name": "Bucks",
+            "price": +165
           },
           {
             "id": "SEL_4",
-            "name": "Messi",
-            "price": 1.3
+            "name": "Celtics",
+            "price": -195
           },
+           {
+        "id": "MKT_3",
+        "name": "Total",
+        "selections": [
           {
             "id": "SEL_5",
-            "name": "Bale",
-            "price": 1.35
-          }
+            "name": "Bucks",
+            "price": "O" 215.5
+          },
+          {
+            "id": "SEL_6",
+            "name": "Celtics",
+            "price": "U" 215.5
+          },
         ]
       }
     ],
-    "name": "Real Madrid vs Barcelona"
+    "name": "Bucks at Celtics"
   },
   {
     "id": "EVT_2",
     "markets": [
       {
-        "id": "MKT_3",
-        "name": "Team to Win",
+        "id": "MKT_4",
+        "name": "Spread",
         "selections": [
           {
-            "id": "SEL_6",
-            "name": "Atletico",
-            "price": 1.4
+            "id": "SEL_7",
+            "name": "Warriors",
+            "price": -2
           },
           {
-            "id": "SEL_7",
-            "name": "Malaga",
-            "price": 3.05
-          }
+            "id": "SEL_8",
+            "name": "Grizzlies",
+            "price": +2
+          },
+           {
+        "id": "MKT_5",
+        "name": "Moneyline",
+        "selections": [
+          {
+            "id": "SEL_9",
+            "name": "Warriors",
+            "price": -130
+          },
+          {
+            "id": "SEL_10",
+            "name": "Grizzlies",
+            "price": +110
+          },
+           {
+        "id": "MKT_6",
+        "name": "Total",
+        "selections": [
+          {
+            "id": "SEL_11",
+            "name": "Warriors",
+            "price": "O" 227.5
+          },
+          {
+            "id": "SEL_12",
+            "name": "Grizzlies",
+            "price": "U" 227.5
+          },
         ]
       }
     ],
-    "name": "Atletico Madrid vs Malaga"
+    "name": "Warriors at Grizzlies"
   },
   {
     "id": "EVT_3",
